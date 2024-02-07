c58618000481243a3919397164877064e4124cbc
ydkim <yedami93@gmail.com>
2024-02-07 17:44:07 +0900

    feat - Naver Map 세팅 테스트

--- README.md
+++ README.md
@@ -5,8 +5,12 @@
   <img src="https://img.shields.io/badge/Typescript-3178C6?style=flat-square&logo=Typescript&logoColor=white"/>
   <img src="https://img.shields.io/badge/Tailwind CSS-06B6D4?style=flat-square&logo=Tailwind CSS&logoColor=white"/>
   <br>
-  
   <img src="https://img.shields.io/badge/Github-181717?style=flat-square&logo=github&logoColor=white">
   <img src="https://img.shields.io/badge/Git-F05032?style=flat-square&logo=git&logoColor=white">
   <br>
 </div>
+
+## Local Server
+```
+npm run start
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,25 @@
-import React from 'react';
-// import {
-//   Container as MapDiv, NaverMap, Marker, useNavermaps,
-// } from 'react-naver-maps';
-
-// function NaverMapComponent() {
-//   const naverMaps = useNavermaps();
-//   return (
-//     <NaverMap
-//       defaultCenter={new naverMaps.LatLng(37.3595704, 127.105399)}
-//       defaultZoom={15}
-//     >
-//       <Marker
-//         defaultPosition={new naverMaps.LatLng(37.3595704, 127.105399)}
-//       />
-//     </NaverMap>
-//   );
-// }
+import React, { useState } from 'react';
+import AppRouter from './components/Router.tsx';
+import AppHeader from './components/AppHeader.tsx';
 
 function App() {
+  const [init] = useState(true);
   return (
-    // <MapDiv
-    //   style={{
-    //     width: '100%',
-    //     height: '600px',
-    //   }}
-    // >
-    //   <NaverMapComponent />
-    // </MapDiv>
-    <div>
-      <h2 className="text-blue-500 text-xl font-bold">Hello, React!</h2>
-      <p className="text-lg font-medium">Hello, Typescript!</p>
-    </div>
+    <>
+      <AppHeader />
+      {init ? (
+        // <span />
+        <AppRouter />
+      ) : (
+        'Initializing...'
+      )}
+      <footer>
+        &copy;
+        {new Date().getFullYear()}
+        {' '}
+        GeoVa
+      </footer>
+    </>
   );
 }
 
