47b09b8a494c036e1ee8a5ee767d6baa95e401ff
ydkim <yedami93@gmail.com>
2024-02-08 11:58:45 +0900

    feat - 헤더 및 메인 페이지 기본 작업 중

---
+++ src/components/NaverMaps.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import { NaverMapsProvider } from 'react-naver-maps';
+
+function NaverMaps({
+  // children,
+}) {
+  return (
+    <>
+    </>
+    // <NaverMapsProvider
+    //   ncpClientId="wxm74y5h9u"
+    // >
+    //   {children}
+    // </NaverMapsProvider>
+  );
+}
+
+NaverMaps.defaultProps = {
+  children: null,
+};
+
+export default NaverMaps;
